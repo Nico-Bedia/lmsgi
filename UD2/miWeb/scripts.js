ab1810c26f73ba6649a364b97aa576302325f056
Nico-Bedia <nbedia01@educantabria.es>
2025-01-20 16:56:02 +0100

    modificaciones 20/01/2025

---
+++ UD2/miWeb/scripts.js
@@ -0,0 +1,7 @@
+// Script para el modo oscuro
+const darkModeToggle = document.querySelector('.dark-mode-toggle');
+const body = document.body;
+
+darkModeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+});
